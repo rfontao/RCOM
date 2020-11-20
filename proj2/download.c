62eeb72a79cad0e5f5c914b77fcc1eadf1f015b0
ricardofontao2000 <up201806317@fe.up.pt>
2020-11-20 19:26:08 +0000

    Can now connect to server. segfault on file download

--- proj2/download.c
+++ proj2/download.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define CONNECTION_PORT 21
+#define ANONYMOUS_PASSWORD "1234"
+
 typedef struct {
     char* host_name;
     char* url;
@@ -15,6 +18,9 @@ typedef struct {
     char* username;
     char* password;
     int anonymous;
+    int sockfd;
+    int data_port;
+    int datafd;
 } connection;
 
 void init_connection(connection *conn) {
@@ -24,6 +30,9 @@ void init_connection(connection *conn) {
     conn->username = NULL;
     conn->password = NULL;
     conn->anonymous = -1;
+    conn->sockfd = -1;
+    conn->data_port = -1;
+    conn->datafd = -1;
 }
 
 void cleanup(connection *conn){
@@ -37,6 +46,10 @@ void cleanup(connection *conn){
         free(conn->username);
     if(conn->password != NULL)
         free(conn->password);
+    if(conn->sockfd != -1)
+        close(conn->sockfd);
+    if(conn->datafd != -1)
+        close(conn->sockfd);
 }
 
 void print_connection(connection* conn){
@@ -51,9 +64,7 @@ void print_connection(connection* conn){
     } else {
         printf("Username: %s\n", conn->username);
         printf("Password: %s\n", conn->password);
-
     }
-
     printf("------------------\n");
 }
 
@@ -84,7 +95,7 @@ int parse_input(connection *conn, char *input) {
     if(token == NULL) 
             return -1;
     if(strcmp(token, "ftp:") != 0){
-        perror("Not ftp protocol\n");
+        fprintf(stderr, "Not ftp protocol\n");
         return -1;
     }
 
@@ -166,9 +177,145 @@ int parse_input(connection *conn, char *input) {
     return 0;
 }
 
+int parse_pasv_response(connection *conn, char* message){
+    char* messageptr;
+    strtok_r(message, ",", &messageptr);
+    strtok_r(NULL, ",", &messageptr);
+    strtok_r(NULL, ",", &messageptr);
+    strtok_r(NULL, ",", &messageptr);
+
+    char* l1_str = strtok_r(NULL, ",", &messageptr);
+    char* l2_str = strtok_r(NULL, ",", &messageptr);
+
+    int L1 = atoi(l1_str);
+    int L2 = atoi(l2_str);
+
+    conn->data_port = L1 * 256 + L2;
+
+    // printf("%d\n", conn->data_port);
+    return 0;
+}
+
+int make_connection(connection *conn){
+
+	struct sockaddr_in server_addr;
+	
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(conn->host_ip);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(CONNECTION_PORT);		    /*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((conn->sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+        fprintf(stderr, "Failed to open socket\n");
+        return -1;
+    }
+	/*connect to the server*/
+    if(connect(conn->sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        fprintf(stderr, "Failed to connect to server\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int make_data_connection(connection *conn){
+
+	struct sockaddr_in server_addr;
+	
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(conn->host_ip);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(conn->data_port);		    /*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((conn->datafd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+        fprintf(stderr, "Failed to open socket\n");
+        return -1;
+    }
+	/*connect to the server*/
+    if(connect(conn->datafd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        fprintf(stderr, "Failed to connect to server\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int send_message(connection *conn, char* message){
+    if(send(conn->sockfd, message, strlen(message), 0) < 0){
+        return -1;
+    }
+	printf("Sent %ld bytes: %s\n", strlen(message), message);
+    return 0;
+}
+
+int read_message(connection *conn, char* message){
+    int read_bytes = -1;
+    do {
+        read_bytes = recv(conn->sockfd, message, 4096, 0);
+        message[read_bytes] = '\0';
+        if(read_bytes < 0){
+            return -1;
+        }
+        // printf("Received %ld bytes: %s\n", strlen(message), message);
+        // printf("Char: %c\n", message[3]);
+        printf("%s", message);
+    } while (read_bytes > 3 && message[3] == '-');
+
+    return read_bytes;
+}
+
+int read_data(connection *conn){
+
+    
+	FILE* fd;
+    if((fd = fopen(conn->url, "w")) < 0){
+        fprintf(stderr, "Error printing file\n");
+        return -1;
+    }
+
+    int i = 0;
+    int c;
+    int read_bytes = -1;
+    char* message = (char*)malloc(sizeof(char) * 200);
+    int cur_size = 200;
+    while(read_bytes != 0) {
+        read_bytes = recv(conn->datafd, &c, 1, 0);
+        if(read_bytes < 0){
+            return -1;
+        }
+        if(i == cur_size){
+            message = (char*)realloc(message, cur_size * 2);
+            cur_size *= 2;
+        }
+        message[i] = c;
+        i += read_bytes;
+        printf("%d\n", cur_size);
+        printf("%d\n", i);
+        // printf("Received %ld bytes: %s\n", strlen(message), message);
+        // printf("Char: %c\n", message[3]);
+        // printf("%s", message);
+    } 
+
+    printf("Hello");
+
+    if(fwrite(message, 1, i - 1, fd) < 0){
+		perror("Error writing to file\n");
+		return -1;
+	}
+
+    free(message);
+    fclose(fd);
+    return 0;  
+
+}
+
 int main(int argc, char **argv){
 
-    if (argc != 2) {
+    if(argc != 2) {
         fprintf(stderr, "usage: download ftp://[<user>:<password>@]<host>/<url-path>\n");
         exit(-1);
     }
@@ -192,6 +339,110 @@ int main(int argc, char **argv){
     }
 
     print_connection(&conn);
+
+    if(make_connection(&conn) < 0){
+        fprintf(stderr, "Error making connection\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+    
+    //Does not work everytime
+    //Greeting
+    char message[4096];
+    if(read_message(&conn, message) < 0 && message[0] != '2'){
+        fprintf(stderr, "Error reading greeting\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    char user_msg[256];
+    strcpy(user_msg, "user ");
+    if(conn.anonymous == 1)
+        strcat(user_msg, "anonymous\n");
+    else {
+        strcat(user_msg, conn.username);
+        strcat(user_msg, "\n");
+    }
+    
+    //Send username
+    if(send_message(&conn, user_msg) < 0){
+        fprintf(stderr, "Failed sending username\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    //Read password prompt
+    if(read_message(&conn, message) < 0 && message[0] != '3'){
+        fprintf(stderr, "Error reading\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+
+    char password_msg[256];
+    strcpy(password_msg, "pass ");
+    if(conn.anonymous == 1){
+        strcat(password_msg, ANONYMOUS_PASSWORD);
+        strcat(password_msg, "\n");
+    } else {
+        strcat(password_msg, conn.password);
+        strcat(password_msg, "\n");
+    }
+    
+    //Send username
+    if(send_message(&conn, password_msg) < 0){
+        fprintf(stderr, "Failed sending password\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    //Password reponse
+    if(read_message(&conn, message) < 0 && message[0] != '2'){
+        fprintf(stderr, "Error reading\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    //Enter passive
+    if(send_message(&conn, "pasv\n") < 0){
+        fprintf(stderr, "Failed sending pasv command\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    //Read pasv response
+    if(read_message(&conn, message) < 0 && message[0] != '2'){
+        fprintf(stderr, "Error reading\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    parse_pasv_response(&conn, message);
+
+    if(make_data_connection(&conn) < 0){
+        fprintf(stderr, "Error making connection\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+
+    //Enter passive
+    char retrieve_msg[256];
+    strcpy(retrieve_msg, "retr ");
+    strcat(retrieve_msg, conn.url);
+    strcat(retrieve_msg, "\n");
+    if(send_message(&conn, retrieve_msg) < 0){
+        fprintf(stderr, "Failed sending pasv command\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
+    if(read_data(&conn) < 0){
+        fprintf(stderr, "Error reading\n");
+        cleanup(&conn);
+        exit(-1);
+    }
+
 
     cleanup(&conn);
 
